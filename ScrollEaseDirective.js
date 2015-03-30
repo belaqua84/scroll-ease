5e0a854f5acfea30353ba60993ebc0dc7a00e6e8
Tracy Hamer <tracy.hamer@gmail.com>
2015-03-29 20:00:35 -0700

    Ran through a js linter

--- ScrollEaseDirective.js
+++ ScrollEaseDirective.js
@@ -41,7 +41,7 @@
                         };
                         animateScroll();
                     });
-                };
+                }
 
                 //t = current time
                 //b = start value
@@ -95,13 +95,13 @@
                     return -c/2 * (Math.cos(Math.PI*t/d) - 1) + b;
                 };
                 Math.easeInExpo = function (t, b, c, d) {
-                    return (t==0) ? b : c * Math.pow(2, 10 * (t/d - 1)) + b;
+                    return (t===0) ? b : c * Math.pow(2, 10 * (t/d - 1)) + b;
                 };
                 Math.easeOutExpo = function (t, b, c, d) {
                     return (t==d) ? b+c : c * (-Math.pow(2, -10 * t/d) + 1) + b;
                 };
                 Math.easeInOutExpo = function (t, b, c, d) {
-                    if (t==0) return b;
+                    if (t===0) return b;
                     if (t==d) return b+c;
                     if ((t/=d/2) < 1) return c/2 * Math.pow(2, 10 * (t - 1)) + b;
                     return c/2 * (-Math.pow(2, -10 * --t) + 2) + b;
@@ -118,21 +118,21 @@
                 };
                 Math.easeInElastic = function (t, b, c, d) {
                     var s=1.70158;var p=0;var a=c;
-                    if (t==0) return b;  if ((t/=d)==1) return b+c;  if (!p) p=d*.3;
+                    if (t===0) return b;  if ((t/=d)==1) return b+c;  if (!p) p=d*.3;
                     if (a < Math.abs(c)) { a=c; var s=p/4; }
                     else var s = p/(2*Math.PI) * Math.asin (c/a);
                     return -(a*Math.pow(2,10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )) + b;
                 };
                 Math.easeOutElastic = function (t, b, c, d) {
                     var s=1.70158;var p=0;var a=c;
-                    if (t==0) return b;  if ((t/=d)==1) return b+c;  if (!p) p=d*.3;
+                    if (t===0) return b;  if ((t/=d)==1) return b+c;  if (!p) p=d*.3;
                     if (a < Math.abs(c)) { a=c; var s=p/4; }
                     else var s = p/(2*Math.PI) * Math.asin (c/a);
                     return a*Math.pow(2,-10*t) * Math.sin( (t*d-s)*(2*Math.PI)/p ) + c + b;
                 };
                 Math.easeInOutElastic = function (t, b, c, d) {
                     var s=1.70158;var p=0;var a=c;
-                    if (t==0) return b;  if ((t/=d/2)==2) return b+c;  if (!p) p=d*(.3*1.5);
+                    if (t===0) return b;  if ((t/=d/2)==2) return b+c;  if (!p) p=d*(.3*1.5);
                     if (a < Math.abs(c)) { a=c; var s=p/4; }
                     else var s = p/(2*Math.PI) * Math.asin (c/a);
                     if (t < 1) return -.5*(a*Math.pow(2,10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )) + b;
@@ -205,7 +205,7 @@
                 };           
             }      
         }
-    };
+    });
 }());
 
 /*TERMS OF USE - EASING EQUATIONS 
